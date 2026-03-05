01778bc03ab642152a08afcdc2b196419fdfa22a
Akhlakur Rahman <ssrmm2000@gmail.com>
2026-03-05 15:30:31 +0600

    Hero section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <div className="inter-font">
+      <div className="inter-font bg-[#F5F5F5]">
         <Navbar></Navbar>
+        <Hero></Hero>
       </div>
     </>
   );
---
+++ src/components/Hero.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import patternimg from "../assets/vector1.png";
+
+const Hero = () => {
+  return (
+    <div className="flex items-center justify-center gap-8 px-[4%] py-10 md:py-20">
+      
+      {/* In Progress */}
+      <div className="relative flex-1 bg-linear-to-r from-[#6a1cd7] to-[#9d70dd] h-56 rounded-xl flex flex-col items-center justify-center text-white overflow-hidden py-37">
+        
+        {/* left pattern */}
+        <img
+          src={patternimg}
+          alt=""
+          className="absolute top-0 left-0 w-92"
+        />
+
+        {/* right pattern */}
+        <img
+          src={patternimg}
+          alt=""
+          className="absolute top-0 right-0 w-92 scale-x-[-1]"
+        />
+
+        <p className="text-2xl">In-Progress</p>
+        <p className="text-6xl font-semibold mt-4">0</p>
+      </div>
+
+      {/* Resolved */}
+      <div className="relative flex-1 bg-linear-to-r from-green-400 to-teal-600 h-56 rounded-xl flex flex-col items-center justify-center text-white overflow-hidden py-37">
+        
+        <img
+          src={patternimg}
+          alt=""
+          className="absolute top-0 left-0 w-92"
+        />
+
+        <img
+          src={patternimg}
+          alt=""
+          className="absolute top-0 right-0 w-92 scale-x-[-1]"
+        />
+
+        <p className="text-2xl">Resolved</p>
+        <p className="text-6xl font-semibold mt-4">0</p>
+      </div>
+
+    </div>
+  );
+};
+
+export default Hero;
